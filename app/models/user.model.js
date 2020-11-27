39160b0622bd1d8cb42dbb8951e778d3d06e1463
kirillshevchenk8 <kirillshevchenkov@protonmail.com>
2020-11-27 20:50:30 +0700

    initial commit

---
+++ app/models/user.model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const User = mongoose.model(
+  "User",
+  new mongoose.Schema({
+    fullname:String,
+    username: String,
+    birthday: String,
+    phonenumber: String,
+    email: String,
+    password: String,
+    roles: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Role"
+      }
+    ]
+  })
+);
+
+module.exports = User;
